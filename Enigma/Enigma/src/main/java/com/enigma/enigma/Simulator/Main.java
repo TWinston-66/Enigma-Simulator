7c94897ae4ed5075a85f3ff0ea6ea9706efae74d
Winston <bradymanthompson@gmail.com>
2023-04-13 11:32:40 -0600

    rotor selector + plugboard screen

--- Enigma/Enigma/src/main/java/com/enigma/enigma/Simulator/Main.java
+++ Enigma/Enigma/src/main/java/com/enigma/enigma/Simulator/Main.java
@@ -1,9 +1,13 @@
 package com.enigma.enigma.Simulator;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -26,6 +30,15 @@ public class Main extends Application {
     // Glow-board Parameters
     double keyPadding = 50;
     double keyRadius = 25;
+
+    // Rotor Parameters
+    double rotorMenuPadding = 300;
+    double rotorMenuY = 150;
+    double rotorMenuOffset = 75;
+    double rotorMenuFontSize = 17;
+
+    // Plugboard button parameters
+    double plugButtonFontSize = 17;
 
     Enigma enigma = new Enigma(new String[] {"VII", "V", "IV"}, "B", new int[] {10,5,12}, new int[] {1,2,3}, "AD FT WH JO PN");
 
@@ -83,8 +96,83 @@ public class Main extends Application {
             lastX = x + lastX;
         }
 
+
+        ComboBox<String> rotor1 = new ComboBox<>(FXCollections.observableArrayList(
+                "I",
+                "II",
+                "III",
+                "IV",
+                "V"
+        ));
+
+        ComboBox<String> rotor2 = new ComboBox<>(FXCollections.observableArrayList(
+                "I",
+                "II",
+                "III",
+                "IV",
+                "V"
+        ));
+
+        ComboBox<String> rotor3 = new ComboBox<>(FXCollections.observableArrayList(
+                "I",
+                "II",
+                "III",
+                "IV",
+                "V"
+        ));
+
+
+        rotor1.setValue("I");
+        rotor2.setValue("II");
+        rotor3.setValue("III");
+
+        group.getChildren().add(rotor1);
+        group.getChildren().add(rotor2);
+        group.getChildren().add(rotor3);
+
+        rotor1.setLayoutX(rotorMenuOffset);
+        rotor1.setLayoutY(rotorMenuY);
+
+        rotor2.setLayoutX(rotorMenuOffset + rotorMenuPadding);
+        rotor2.setLayoutY(rotorMenuY);
+
+        rotor3.setLayoutX(rotorMenuOffset + (2 * rotorMenuPadding));
+        rotor3.setLayoutY(rotorMenuY);
+
+        rotor1.setStyle("-fx-font-size: 15px;");
+
+        // set the background color of the dropdown menu
+        rotor1.setStyle("-fx-background-color: gray; -fx-font-size: " + rotorMenuFontSize + "px; -fx-text-fill: black;");
+        rotor2.setStyle("-fx-background-color: gray; -fx-font-size: " + rotorMenuFontSize + "px; -fx-text-fill: black;");
+        rotor3.setStyle("-fx-background-color: gray; -fx-font-size: " + rotorMenuFontSize + "px; -fx-text-fill: black;");
+
+        Label rotor1Heading = new Label("Rotor 1");
+        rotor1Heading.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
+        rotor1Heading.setLayoutX(rotorMenuOffset);
+        rotor1Heading.setLayoutY(rotorMenuY - 40);
+        group.getChildren().add(rotor1Heading);
+
+        Label rotor2Heading = new Label("Rotor 2");
+        rotor2Heading.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
+        rotor2Heading.setLayoutX(rotorMenuOffset + rotorMenuPadding);
+        rotor2Heading.setLayoutY(rotorMenuY - 40);
+        group.getChildren().add(rotor2Heading);
+
+        Label rotor3Heading = new Label("Rotor 3");
+        rotor3Heading.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
+        rotor3Heading.setLayoutX(rotorMenuOffset + (2 * rotorMenuPadding));
+        rotor3Heading.setLayoutY(rotorMenuY - 40);
+        group.getChildren().add(rotor3Heading);
+
+
+        rotor1.setOnAction(event -> {
+            String selectedValue = rotor1.getValue();
+            System.out.println("Rotor 1 value: " + selectedValue);
+        });
+
         Scene scene = new Scene(group, width, height, Color.DARKSLATEGRAY);
         scene.setFill(Color.GRAY);
+
 
 
         final boolean[] isExecuted = {false};
@@ -109,13 +197,39 @@ public class Main extends Application {
         });
 
         scene.setOnKeyReleased(event -> {
-            //String key = event.getText();
-            //int letterIndex = letterOrder.indexOf(key.toUpperCase());
             if (pressedIndex.get() != -1) {
                 circles[pressedIndex.get()].setFill(Color.LIGHTSLATEGRAY);
                 isExecuted[0] = false;
             }
         });
+
+        Group plugboardGroup = new Group();
+        Scene plugboard = new Scene(plugboardGroup, width, height, Color.DARKSLATEGRAY);
+        plugboard.setFill(Color.GRAY);
+
+
+        Button button = new Button("Plugboard");
+        button.setLayoutX(650);
+        button.setLayoutY(25);
+        group.getChildren().add(button);
+        button.setStyle("-fx-background-color: gray; -fx-font-size: " + plugButtonFontSize + "px; -fx-text-fill: black;");
+        button.setOnAction(event -> {
+            stage.setScene(plugboard);
+            stage.setTitle("Enigma Simulator");
+            stage.show();
+        });
+
+        Button back = new Button("Back");
+        back.setLayoutX(700);
+        back.setLayoutY(25);
+        plugboardGroup.getChildren().add(back);
+        back.setStyle("-fx-background-color: gray; -fx-font-size: " + plugButtonFontSize + "px; -fx-text-fill: black;");
+        back.setOnAction(event -> {
+            stage.setScene(scene);
+            stage.setTitle("Enigma Simulator");
+            stage.show();
+        });
+
 
         // Create the scene and set it on the primary stage
         stage.setScene(scene);
